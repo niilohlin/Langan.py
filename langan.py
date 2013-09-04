70b27a69433e4a4e5329f0c06b9246250a2f18ba
Niil Öhlin <niil.94@hotmail.com>
2013-09-04 22:14:18 +0200

    Made the guessing algorithm

--- langan.py
+++ langan.py
@@ -23,20 +23,59 @@ def _freq(text):
 
 def _compare(first, second):
 	res = {}
-	for fk, fv in first.iteritems:
+	for fk, fv in first.iteritems():
 		if fk in second:
 			res[fk] = abs(fv - second[fk])
-	for sk, sv in second.iteritems:
+		else:
+			res[fk] = fv
+
+	for sk, sv in second.iteritems():
 		if sk in first and not sk in res:
-			res[sk]= abs(sv - first[sk])
+			res[sk] = abs(sv - first[sk])
+		else:
+			res[sk] = sv
 	return res
 
 def _sum(compared):
 	sum = 0
-	for v in compared.itervalues:
+	for v in compared.itervalues():
 		sum += v
 	return sum
 
-def _closest(listof, db):
-	for lang in listof:
+def _map_hash_values(fun, myhash):
+	res = {}
+	for k, v in myhash.iteritems():
+		res[k] = fun(v)
+	return res
 
+def _percentage(freq):
+	s = float(_sum(freq))
+	return _map_hash_values(lambda v : v / s, freq)
+
+def closest(freq, db):
+	"""Tries guess of which language the given freq
+	is corresponding to. Returns a list of the language
+	and the error rate. Smaller == better.
+	"""
+	freq = _percentage(freq)
+	for i in xrange(len(db)):
+		db[i][1] = _percentage(db[i][1])
+	res = []
+	for lang, langfreq in db:
+		res.append([lang, _compare(freq, langfreq)])
+	res = map(lambda x: [x[0],_sum(x[1])], res)
+	smallest = res[0]
+	for i in res:
+		if i[1] < smallest[1]:
+			smallest = i
+	return smallest
+
+def guess(text, db):
+	"""Tries to guess the language of a given text"""
+	return closest(analayze(text), db)
+
+
+
+db = [['sv', analyze("okej nu blir det lite sveska i huset")],
+		['en', analyze("okay well this is a little bit of english")]]
+freq = analyze("och nu har vi lite sveska att leka med igen")
