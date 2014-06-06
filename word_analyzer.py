3a1110295c2995ca827c753f6b08a07d0166a797
niil <niil.94@hotmail.com>
2014-06-06 15:07:14 +0200

    code cleanup in wordanalayzer

--- word_analyzer.py
+++ word_analyzer.py
@@ -3,19 +3,17 @@
 
 
 def analyze(text):
-	text = text.lower()
-	for c in ':;,.!?[]7531902468':
-		text = text.replace(c, '')
-	res = {}
-	for word in text.split(' '):
-		if word in res:
-			res[word] += 1
-		else:
-			res[word] = 1
-	tlist = []
-	for k, v in res.iteritems():
-		tlist.append((k, v))
-	tlist.sort(key=lambda tup: tup[1])
-	return tlist
+    text = text.lower()
+
+    for c in ':;,.!?[]7531902468':
+        text = text.replace(c, '')
+    res = {}
+    for word in text.split(' '):
+        res.setdefault(word, 0)
+        res[word] += 1
+
+    tlist = [(k, v) for k, v in res.iteritems()]
+    tlist.sort(key=lambda tup: tup[1])
+    return tlist
 
 
